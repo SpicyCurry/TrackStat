9423fb976fd1bb7dc714393806172788600fa13e
Claes Gustaf Andersson <claes.gustav.andersson@gmail.com>
2015-12-02 00:13:32 +0100

    Added placeholder function for steamAuth in index, as well as make index pretty with style.css. Also added privacy policy and end user agreement cos steam wants them in order to get our own API Key

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require ('steamauth/steamauth.php');
+?>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>TrackStat</title>
+
+	<link rel = "stylesheet" type = "text/css" href="style.css"/>
+</head>
+<body>
+<div id="content" align="center">
+	<h1>TrackStat</h1>
+	<a href="endUserAgreement.html">End User Agreement</a> -
+	<a href="privacy.html">Privacy Policy </a>
+	<br><br>
+	<form action="placeholder.php" method="post"> <input type="image" src="http://cdn.steamcommunity.com/public/images/signinthroughsteam/sits_large_border.png"></form>
+
+</div>
+</body>
+</html>
---
+++ placeholder.php
@@ -0,0 +1,2 @@
+<?php
+echo "<div align='center' style='padding-top: 200'>No Steam API Key!</div>";
