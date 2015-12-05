cbba7a054ee73e67d39524d731435340846f6461
root <root@unbunserver01.skelabb.ltu.se>
2015-12-05 03:10:47 +0100

    missed some capitalisation in user. Done for today mofo!

--- database/user.php
+++ database/user.php
@@ -9,13 +9,13 @@ function checkUserPDO()
 	$time = time();
 	try
 	{
-		$dbh = new PDO('mysql:host=localhost;dbname=TrackStatDB', "root", "");
-		$selectStmt =  $dbh->prepare("SELECT * FROM user WHERE SteamID64=:ID");
+		$dbh = new PDO('mysql:host=localhost;dbname=TrackStatDB', "server", "10blowjobsfromAlex");
+		$selectStmt =  $dbh->prepare("SELECT * FROM User WHERE SteamID64=:ID");
 		$selectStmt->bindParam(":ID", $ID);
 		$selectStmt->execute();
 		if ($selectStmt->rowCount() == 1)
 		{
-			$updateStmt = $dbh->prepare("UPDATE user SET LastLogin=:time WHERE SteamID64=:ID");
+			$updateStmt = $dbh->prepare("UPDATE User SET LastLogin=:time WHERE SteamID64=:ID");
 			$updateStmt->bindparam(":ID", $ID);
 			$updateStmt->bindParam(":time", $time);
 			$updateStmt->execute();
@@ -23,7 +23,7 @@ function checkUserPDO()
 		}
 		else
 		{
-			$insertStmt = $dbh->prepare("INSERT INTO user (SteamID64, LastLogin) VALUES (:ID,:time)");
+			$insertStmt = $dbh->prepare("INSERT INTO User (SteamID64, LastLogin) VALUES (:ID,:time)");
 			$insertStmt->bindparam(":ID", $ID);
 			$insertStmt->bindParam(":time", $time);
 			$insertStmt->execute();
